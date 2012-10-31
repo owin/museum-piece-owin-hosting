9d9e5c2be471647ff4242b0c207e4e07ae1bad43
Louis DeJardin <louis.dejardin@gmail.com>
2012-10-31 13:22:46 -0700

    Pinning Owin.nupkg to 1.0
    Also updating license/notice packaging
    Redistrubuting license and notice in sources distribution

--- src/main/Owin.Loader/Properties/AssemblyInfo.cs
+++ src/main/Owin.Loader/Properties/AssemblyInfo.cs
@@ -1,6 +1,22 @@
+// Licensed under one or more contributor license agreements.  
+// See the NOTICE.txt file distributed with this work for 
+// additional information regarding copyright ownership.  The 
+// copyright owners license this file to you under the Apache 
+// License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain 
+// a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -28,7 +44,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.5")]
-[assembly: AssemblyVersion("0.5")]
-[assembly: AssemblyFileVersion("0.5")]
+// [assembly: AssemblyVersion("0.6.1")]
+[assembly: AssemblyVersion("0.6.1")]
+[assembly: AssemblyFileVersion("0.6.1")]
 [assembly: CLSCompliant(false)]
